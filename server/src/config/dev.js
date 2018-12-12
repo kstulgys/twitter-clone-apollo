f20b568d075b4c4d21c3908b791b881ca7e33257
kstulgys <karolis.stulgys@gmail.com>
2018-12-12 16:18:01 +1100

    'wip'

--- server/src/config/dev.js
+++ server/src/config/dev.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
+
 export const config = {
   port: 4000,
   jwt: 'helloworld',
   jwtExp: '100d',
-  dbUrl:
-    'mongodb://tmdb:tmdb123@tmdb-server-dev-shard-00-00-uyxf0.mongodb.net:27017,tmdb-server-dev-shard-00-01-uyxf0.mongodb.net:27017,tmdb-server-dev-shard-00-02-uyxf0.mongodb.net:27017/test?ssl=true&replicaSet=tmdb-server-dev-shard-0&authSource=admin&retryWrites=true',
+  dbUrl: process.env.MONGO_DB_DEV,
 }
--- server/src/config/prod.js
+++ server/src/config/prod.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 export const config = {
   port: process.env.PORT,
   jwt: process.env.JWT_SECRET,
