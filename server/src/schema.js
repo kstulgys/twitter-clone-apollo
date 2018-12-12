0a2cad4cb32ecd0baa075f72852edba0cd8ea744
kstulgys <karolis.stulgys@gmail.com>
2018-12-12 16:07:13 +1100

    'wip'

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,67 +1,67 @@
-import { gql } from 'apollo-server'
+// import { gql } from 'apollo-server'
 
-const typeDefs = gql`
-  type Query {
-    # listMoviesWithParameters(input: MovieInput): MoviesPage
-    listGenres: [Genre]!
-    listUsers: [User]!
-    me: User
-    infiniteScrollMovies(offset: Int, limit: Int): MoviesPage
-  }
+// const typeDefs = gql`
+//   type Query {
+//     # listMoviesWithParameters(input: MovieInput): MoviesPage
+//     listGenres: [Genre]!
+//     listUsers: [User]!
+//     me: User
+//     infiniteScrollMovies(offset: Int, limit: Int): MoviesPage
+//   }
 
-  type MoviesPage {
-    movies: [Movie]!
-    hasMore: Boolean
-    newOffset: Int
-  }
+//   type MoviesPage {
+//     movies: [Movie]!
+//     hasMore: Boolean
+//     newOffset: Int
+//   }
 
-  type Mutation {
-    login(email: String): String
-    addToWatched(id: Int): Int!
-    addToWatchLater(id: Int): Int!
-    deleteFromWatched(id: Int): Int!
-    deleteFromWatchLater(id: Int): Int!
-  }
+//   type Mutation {
+//     login(email: String): String
+//     addToWatched(id: Int): Int!
+//     addToWatchLater(id: Int): Int!
+//     deleteFromWatched(id: Int): Int!
+//     deleteFromWatchLater(id: Int): Int!
+//   }
 
-  type Message {
-    message: String
-  }
+//   type Message {
+//     message: String
+//   }
 
-  input MovieInput {
-    genreId: Int
-    yearMin: Int
-    yearMax: Int
-    ratingMin: Int
-    ratingMax: Int
-    runtimeMin: Int
-    runtimeMax: Int
-    pageNum: Int
-    pageSize: Int
-  }
+//   input MovieInput {
+//     genreId: Int
+//     yearMin: Int
+//     yearMax: Int
+//     ratingMin: Int
+//     ratingMax: Int
+//     runtimeMin: Int
+//     runtimeMax: Int
+//     pageNum: Int
+//     pageSize: Int
+//   }
 
-  type Movie {
-    vote_count: Int
-    id: ID
-    genre_ids: [ID]
-    image_url: String
-    title: String
-    vote_average: String
-    release_date: String
-    watchedBy: [User]!
-  }
+//   type Movie {
+//     vote_count: Int
+//     id: ID
+//     genre_ids: [ID]
+//     image_url: String
+//     title: String
+//     vote_average: String
+//     release_date: String
+//     watchedBy: [User]!
+//   }
 
-  type Genre {
-    id: ID
-    name: String
-  }
+//   type Genre {
+//     id: ID
+//     name: String
+//   }
 
-  type User {
-    _id: ID!
-    email: String!
-    avatar: String!
-    watched: [Movie]!
-    watchLater: [Movie]!
-  }
-`
+//   type User {
+//     _id: ID!
+//     email: String!
+//     avatar: String!
+//     watched: [Movie]!
+//     watchLater: [Movie]!
+//   }
+// `
 
-module.exports = typeDefs
+// module.exports = typeDefs
