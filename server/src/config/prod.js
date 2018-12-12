9eef242c25a37f81ec8bc5e60ae342bd5dd1d21f
kstulgys <karolis.stulgys@gmail.com>
2018-12-12 16:11:47 +1100

    'wip'

--- server/src/config/prod.js
+++ server/src/config/prod.js
@@ -1,8 +1,6 @@
 export const config = {
   port: process.env.PORT,
-  secrets: {
-    jwt: process.env.JWT_SECRET,
-    jwtExp: '100d',
-    dbUrl: process.env.MONGO_DB,
-  },
+  jwt: process.env.JWT_SECRET,
+  jwtExp: '100d',
+  dbUrl: process.env.MONGO_DB,
 }
