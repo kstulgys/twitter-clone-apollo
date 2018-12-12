aca6037a6174dafbaf4039c45589a31014e8b404
karolis stulgys <k.stulgys@gmail.com>
2018-12-12 18:37:15 +1100

    wip

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-import { merge } from 'lodash'
-const env = process.env.NODE_ENV || 'development'
-// / base
+require("dotenv").config();
+import { merge } from "lodash";
+const env = process.env.NODE_ENV || "development";
+/// base
 const baseConfig = {
   env,
-  isDev: env === 'development',
-  isProd: env === 'production',
-  isTest: env === 'testing',
-}
+  isDev: env === "development",
+  isProd: env === "production",
+  isTest: env === "testing"
+};
 
-let envConfig = {}
+let envConfig = {};
 
 switch (env) {
-  case 'dev':
-  case 'development':
-    envConfig = require('./dev').config
-    break
-  case 'prod':
-  case 'production':
-    envConfig = require('./prod').config
-    break
+  case "dev":
+  case "development":
+    envConfig = require("./dev").config;
+    break;
+  case "prod":
+  case "production":
+    envConfig = require("./prod").config;
+    break;
 }
 
-export default merge(baseConfig, envConfig)
+export default merge(baseConfig, envConfig);
